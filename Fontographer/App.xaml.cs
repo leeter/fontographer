17a8edd1e90ca57faea9859113a55641310c2897
leeter <leeter@users.noreply.github.com>
2012-03-26 17:38:46 -0600

    commit to repository

---
+++ Fontographer/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Interaction logic for App.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Fontographer
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
