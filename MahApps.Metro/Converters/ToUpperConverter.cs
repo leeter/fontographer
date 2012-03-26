71d7d085b1596e7d14fb30293f816fb98760a194
leeter <leeter@localhost>
2012-03-26 17:39:33 -0600

    Initial commit

---
+++ MahApps.Metro/Converters/ToUpperConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MahApps.Metro.Converters
+{
+    public class ToUpperConverter : MarkupConverter
+    {
+        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value as string;
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                return str.ToUpper();
+            }
+
+            return value;
+        }
+
+        protected override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+
+    public class ToLowerConverter : MarkupConverter
+    {
+        protected override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value as string;
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                return str.ToLower();
+            }
+
+            return value;
+        }
+
+        protected override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ MahApps.Metro/UnsafeNativeMethods.cs
@@ -0,0 +1,42 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="UnsafeNativeMethods.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MahApps.Metro
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+    using System.Security;
+    using System.Text;
+
+    using MahApps.Metro.Native;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    [SuppressUnmanagedCodeSecurity]
+    internal static class UnsafeNativeMethods
+    {
+        [DllImport("dwmapi", PreserveSig = false)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool DwmIsCompositionEnabled();
+
+        [DllImport("dwmapi", PreserveSig = true)]
+        [return: MarshalAs(UnmanagedType.Error)]
+        internal static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, [In] ref MARGINS pMarInset);
+
+        [DllImport("user32")]
+        internal static extern IntPtr DefWindowProc([In] IntPtr hwnd, [In] int msg, [In] IntPtr wParam, [In] IntPtr lParam);
+
+        [DllImport("user32")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetMonitorInfo([In] IntPtr hMonitor, [Out] MONITORINFO lpmi);
+
+        [DllImport("user32")]
+        internal static extern IntPtr MonitorFromWindow([In] IntPtr handle, [In] int flags);
+    }
+}
